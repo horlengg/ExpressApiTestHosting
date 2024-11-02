1e508d4151f7c99e14f8c2a7107813123401075d
Leng <horleng123@gmail.com>
2024-11-02 14:44:06 +0700

    add product route

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ import express from 'express';
 
 const app = express();
 
+const products = [
+    {
+        id : 1,
+        name : "Product 1",
+        description : "This is product 1",
+        price : 100
+    },
+    {
+        id : 2,
+        name : "Product 2",
+        description : "This is product 2",
+        price : 200
+    }
+]
+
 app.get("/",(req,res)=>{
     return res.json({
         message : "Message from express application"
+    })
+})
+app.get("/product",(req,res)=>{
+    return res.status(200).json({
+        message : "fetched product sucessfully",
+        data : products
     })
 })
 
